928c83f275b7280c043edf83d0f3516d97c684b6
bryanmichaelk <bryanmichaelk@gmail.com>
2024-11-19 16:27:14 +0700

    fix: error docker-compose

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => console.log('Server up and running...'));
+app.listen(5000, () => console.log("Server up and running..."));
 
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'UP',
-        message: 'Server is running',
-        timestamp: new Date().toISOString(),
-    });
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    message: "Server is running",
+    timestamp: new Date().toISOString(),
+  });
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}`);
+// Define route for the root path
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the Todo App API!kekekekek');
 });
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is up and running on port ${PORT}`);
+// });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './styles/style.css';
+import "./styles/style.css";
 
 function App() {
-    return (
-        <>
-            <h1 className='text-center bg-blue-50'>Hello World Test PSO</h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className="text-center bg-blue-50">Hello World Test PSO</h1>
+    </>
+  );
 }
 
 export default App;

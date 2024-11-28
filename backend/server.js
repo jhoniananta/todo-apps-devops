3a0e0f0109a81a9880596e78722f5bf535f6ad85
jhoniananta <jhoniananta@gmail.com>
2024-11-28 20:10:01 +0700

    change vite config

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from 'dotenv';
 import 'dotenv/config';
+import todo from './models/todo.js';
 import sql from 'mssql';
-
 
 const server = process.env.AZURE_SQL_SERVER;
 const database = process.env.AZURE_SQL_DATABASE;
@@ -25,6 +25,17 @@ app.use(express.json());
 
 app.listen(5000, () => console.log("Server up and running..."));
 
+// Get the /priorities route from todo.js
+app.get('/priorities', async (req, res) => {
+  try {
+    const priority = await todo.readPriorities();
+    console.log(`priority: ${JSON.stringify(priority)}`);
+    res.status(200).json(priority);
+  } catch (err) {
+    res.status(500).json({ error: err?.message });
+  }
+});
+
 app.get("/get", (req, res) => {
   res.status(200).json({
     status: "UP",
@@ -35,25 +46,13 @@ app.get("/get", (req, res) => {
 
 // Define route for the root path
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Todo App API! Dor');
+    res.status(200).send('Welcome to the Todo App API!');
 });
 
 // const PORT = 5000;
 // app.listen(PORT, () => {
 //     console.log(`Server is up and running on port ${PORT}`);
 // });
-export const noPasswordConfig = {
-  server,
-  port,
-  database,
-  authentication: {
-    type,
-  },
-  options: {
-    encrypt: true,
-    trustServerCertificate: false,
-  },
-};
 
 export const passwordConfig = {
   server,
@@ -69,8 +68,9 @@ export const passwordConfig = {
 async function testConnection() {
   try {
     console.log('Connecting to Azure SQL Database...');
-    const pool = await sql.connect(noPasswordConfig); // Use corrected config
+    const pool = await sql.connect(passwordConfig); // Use corrected config
     console.log('Connection successful!');
+
     await pool.close();
   } catch (err) {
     console.error('Connection failed:', err);

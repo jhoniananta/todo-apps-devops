a6b9ae00086e3d2bed7048f156309e55e5645903
bryanmichaelk <bryanmichaelk@gmail.com>
2024-12-03 19:30:16 +0700

    fix: priority card

--- src/components/card-todo/PriorityBadge.tsx
+++ src/components/card-todo/PriorityBadge.tsx
@@ -4,7 +4,7 @@ interface PriorityBadgeProps {
 
 export function PriorityBadge({ priority }: PriorityBadgeProps) {
   return (
-    <div className={`p-1 text-lg text-center whitespace-normal rounded-xl w-[98px] ${priority=="high" ? 'bg-red-200': priority=="medium"? 'bg-yellow-200': 'bg-green-200'}`}>
+    <div className={`p-1 text-lg text-center whitespace-normal rounded-xl w-[98px] ${priority=="High" ? 'bg-red-200': priority=="Medium"? 'bg-yellow-200': 'bg-green-200'}`}>
       {priority}
     </div>
   );
